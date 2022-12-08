7aab370cb2d942af4ae77d7d5859449ae1ac6e65
Jack Doyle <jackdoyle@Jacks-MacBook-Pro.local>
2022-12-08 14:20:20 +0100

    Added basic html & css

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
-const express = require("express")
+const path = require('path')
+const express = require('express')
 
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
 app.get('', (req, res) => {
-    res.send("Hellow World")
+    res.render('index', {
+        title: 'Get to know each other'
+    })
 })
 
 app.listen(3000, () => {
